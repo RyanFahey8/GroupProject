33604977bb8aeff006fbb64366389baac8831a69
Luke Lenhardt <lukejlenhardt@ou.edu>
2019-11-26 16:37:54 -0600

    adding classes

---
+++ RandomSeatAssigner/RandomSeatAssigner/students.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomSeatAssigner
+{
+    class students
+    {
+        String name { get; set; }
+        String status { get; set; }
+
+        int station { get; set; }
+
+
+    }
+}
---
+++ RandomSeatAssigner/RandomSeatAssigner/workstation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomSeatAssigner
+{
+    class workstation
+    {
+        int number { get; set; }
+        
+        int status { get; set; }
+    }
+}
