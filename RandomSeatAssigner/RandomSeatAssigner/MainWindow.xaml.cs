78b15d4b34b8bbb2e4903b2d09942be9b0a78dd9
Ryan Fahey <Ryan.Fahey@ou.edu>
2019-11-30 18:49:26 -0600

    Added option to enter seat number and broken seats - took out repeating.

--- RandomSeatAssigner/RandomSeatAssigner/MainWindow.xaml.cs
+++ RandomSeatAssigner/RandomSeatAssigner/MainWindow.xaml.cs
@@ -32,15 +32,28 @@ namespace RandomSeatAssigner
 
         private void AssignButton_Click(object sender, RoutedEventArgs e)
         {
+            int seatnum = Convert.ToInt32(SeatCountTB.Text);
             if (File.Exists(FileBox.Text) == true)
             {
                 var lines = File.ReadAllLines(FileBox.Text);
                 Random random = new Random();
                 List<int> availableseats = new List<int>();
-                for(int j = 1; j <= 35; j++)
+                for(int j = 1; j <= seatnum; j++)
                 {
                     availableseats.Add(j);
                 }
+
+                if(BrokenTB.Text.Length>0)
+                {
+                    string brokenlist = BrokenTB.Text;
+                    List<int> brokenseats = brokenlist.Split(',').Select(int.Parse).ToList();
+                    foreach (var brokenseat in brokenseats)
+                    {
+                        availableseats.Remove(Convert.ToInt32(brokenseat));
+                    }
+                }
+
+
                 for (int i = 1; i < lines.Length; i++)
                 {
                     var line = lines[i];
@@ -48,12 +61,15 @@ namespace RandomSeatAssigner
                     string name = column[0];
                     StudentLB.Items.Add(name);
 
-                    int seat = random.Next(35);
-                    if(availableseats.Contains(seat))
+                    int seat;
+                    do
                     {
+                       seat = random.Next(seatnum);
+                    } while (!availableseats.Contains(seat));
+
+
                         SeatLB.Items.Add(seat);
                         availableseats.Remove(seat);
-                    }
                 }
             }
         }
